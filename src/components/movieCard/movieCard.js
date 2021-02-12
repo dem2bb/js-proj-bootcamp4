1107ef2d6510266695b5d50268aa651551003bd7
Lougle <louglescom@gmail.com>
2021-02-12 22:58:31 +0200

    added disable and btn content after click, but localstorage is saving only one movie

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,15 +1,33 @@
 import movieCard from '../../templates/movieCard.hbs';
 const mainRef = document.querySelector('main');
-
+let id = [];
 export function apiMovieCard(movieId) {
+  id = movieId;
   const keyApi = '65999cd4dc4e9b42ad69f2cfa64d7f94';
   const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}&language=en-US`;
   return fetch(baseUrl)
-    .then(res => res.json())
-    .then(movieCardMurkup)
-    .catch(err => console.log(err));
+  .then(res => res.json())
+  .then(movieCardMurkup)
+  .catch(err => console.log(err));
 }
 
 function movieCardMurkup(data) {
   mainRef.innerHTML = movieCard(data);
+  const watched = document.querySelector('.js-watched');
+  const queue = document.querySelector('.js-queue');
+  watched.addEventListener('click', addIntoWatched);
+  queue.addEventListener('click', addIntoQueue);
+}
+
+function addIntoWatched(e) {
+  const btn = e.target;
+  btn.disabled = true;
+  btn.innerText = 'Added to Watched';
+  localStorage.setItem('WatchedId', JSON.stringify(id));
+}
+function addIntoQueue(e) {
+  const btn = e.target;
+  btn.disabled = true;
+  btn.innerText = 'Added to Queue';
+  localStorage.setItem('QueueId', JSON.stringify(id));
 }
