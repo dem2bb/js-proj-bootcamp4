70e932b42f36a3b6fae1236cd678c265b09d982b
Pavel Sakun <axelhomkin@gmail.com>
2021-02-13 15:52:13 +0200

    created developer team page

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './components/example/header.js';
 import './components/header/header.js';
 import './components/popularMovies/fetch.js';
 import './components/main/searchPrint.js';
+import './components/footer/footer.js';
