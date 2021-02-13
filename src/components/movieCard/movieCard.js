0d179317d62f5550a4e6179b42478d948fb160a3
Lougle <louglescom@gmail.com>
2021-02-13 17:42:54 +0200

    changes

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -3,6 +3,7 @@ const mainRef = document.querySelector('main');
 let id = [];
 export function apiMovieCard(movieId) {
   id = movieId;
+  console.log(id);
   const keyApi = '65999cd4dc4e9b42ad69f2cfa64d7f94';
   const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}&language=en-US`;
   return fetch(baseUrl)
