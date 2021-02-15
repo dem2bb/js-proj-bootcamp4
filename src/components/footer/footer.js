ba88ca36eeb45f2d44aa4245ae806fe6ee161d89
Pavel Sakun <axelhomkin@gmail.com>
2021-02-15 13:34:02 +0200

    modal window footer finished

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,18 +2,16 @@ import poky from '../../images/poky.jpg';
 
 const students = document.querySelector('.footer-description__link');
 const main = document.querySelector('.main-cont');
+const body = document.querySelector('body');
 
 students.addEventListener('click', openModal);
 
-const img = '../../images/poky.jpg';
-
-function openModal(event) {
-  event.preventDefault();
+const createDevelopers = () => {
   const markup = `<div class='overlay'>
     <div class='modal'>
         <h2 class='our-team__name'>Development team</h2>
             <ul class='modalWindow-list'>
-                <li class='modalWindow-item'>
+            <li class='modalWindow-item'>
                 <img 
                 src=${poky} 
                 alt="Team lead picture"
@@ -77,18 +75,41 @@ function openModal(event) {
                 width="150"
                 height="150"
                 /><h3 class='teammates-name'>Stanislav Karasyk </h3><span class='our-team__position'>Teammate</span></li>
-            </ul>
+                </ul>
         <button class='button-footer button is-active'>Close</button>
-      </div>
+        </div>
     </div>`;
   main.insertAdjacentHTML('beforeend', markup);
+};
 
-  const closeBtnRef = document.querySelector('.modal > button');
+function openModal(event) {
+  event.preventDefault();
+  createDevelopers();
+
   const modalWindowRef = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+
+  overlay.style.display = 'block';
+  body.style.overflow = 'hidden';
 
   modalWindowRef.addEventListener('click', event => {
     if (event.target.nodeName === 'BUTTON') {
-      modalWindowRef.remove();
+      overlay.remove();
+      body.style.overflow = 'unset';
     }
   });
+
+  body.addEventListener('keydown', escHandler);
+
+  function escHandler(event) {
+    if (event.code === 'Escape') {
+      overlay.remove();
+      body.style.overflow = 'unset';
+      body.removeEventListener('keydown', escHandler);
+    }
+  }
 }
+
+// function closeModal(event) {
+
+// }
