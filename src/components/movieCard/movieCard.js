630dd26cce79d9592a1eea3d5149b25f96fde0ae
Dmytro Orgish <isnl.dd@gmail.com>
2021-02-15 12:53:43 +0200

    languge

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,19 +1,26 @@
+import { languageData } from '../language-set/language-set.js';
 import movieCard from '../../templates/movieCard.hbs';
+import movieCardRu from '../../templates/movieCard.hbs';
+
 const mainRef = document.querySelector('main');
 let id = [];
-export function apiMovieCard(movieId) {
+export function apiMovieCard (movieId) {
   id = movieId;
   console.log(id);
   const keyApi = '65999cd4dc4e9b42ad69f2cfa64d7f94';
-  const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}&language=en-US`;
+  const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}&language=${languageData.fetchLanguage}`;
   return fetch(baseUrl)
-  .then(res => res.json())
-  .then(movieCardMurkup)
-  .catch(err => console.log(err));
+    .then(res => res.json())
+    .then(movieCardMurkup)
+    .catch(err => console.log(err));
 }
 
-function movieCardMurkup(data) {
-  mainRef.innerHTML = movieCard(data);
+function movieCardMurkup (data) {
+  console.log(languageData.language);
+  languageData.language === 'RU'
+    ? (mainRef.innerHTML = movieCardRu(data))
+    : (mainRef.innerHTML = movieCard(data));
+
   const watched = document.querySelector('.js-watched');
   const queue = document.querySelector('.js-queue');
   watched.addEventListener('click', addIntoWatched);
@@ -35,55 +42,65 @@ function movieCardMurkup(data) {
   }
 }
 
-function addIntoWatched(e) {
+function addIntoWatched (e) {
   let getWatchedMovie = putWatched();
   const btn = e.target;
-  btn.innerHTML = (btn.innerHTML === 'Added to Watched') ? btn.innerHTML = 'Add to Watched' : btn.innerHTML = 'Added to Watched';
-  (btn.classList.contains('js-clicked')) ? btn.classList.remove('js-clicked') : btn.classList.add('js-clicked');
+  btn.innerHTML =
+    btn.innerHTML === 'Added to Watched'
+      ? (btn.innerHTML = 'Add to Watched')
+      : (btn.innerHTML = 'Added to Watched');
+  btn.classList.contains('js-clicked')
+    ? btn.classList.remove('js-clicked')
+    : btn.classList.add('js-clicked');
   localStorage.setItem('WatchedId', JSON.stringify(getWatchedMovie.movie));
 }
-function addIntoQueue(e) {
+function addIntoQueue (e) {
   let getQueueMovie = putQueue();
   const btn = e.target;
-  btn.innerHTML = (btn.innerHTML === 'Added to Queue') ? btn.innerHTML = 'Add to Queue' : btn.innerHTML = 'Added to Queue';
-  (btn.classList.contains('js-clicked')) ? btn.classList.remove('js-clicked') : btn.classList.add('js-clicked');
+  btn.innerHTML =
+    btn.innerHTML === 'Added to Queue'
+      ? (btn.innerHTML = 'Add to Queue')
+      : (btn.innerHTML = 'Added to Queue');
+  btn.classList.contains('js-clicked')
+    ? btn.classList.remove('js-clicked')
+    : btn.classList.add('js-clicked');
   localStorage.setItem('QueueId', JSON.stringify(getQueueMovie.movie));
 }
-function getQueue() {
+function getQueue () {
   const movieStorage = localStorage.getItem('QueueId');
   if (movieStorage !== null) {
     return JSON.parse(movieStorage);
   }
   return [];
 }
-function getWatched() {
+function getWatched () {
   const movieStorage = localStorage.getItem('WatchedId');
   if (movieStorage !== null) {
     return JSON.parse(movieStorage);
   }
   return [];
 }
-function putWatched() {
+function putWatched () {
   let movie = getWatched();
   let pushMovie = false;
   const index = movie.indexOf(id);
-  if(index === -1) {
+  if (index === -1) {
     movie.push(id);
     pushMovie = true;
-  }else{
+  } else {
     movie.splice(index, 1);
   }
-  return {movie, pushMovie }
+  return { movie, pushMovie };
 }
-function putQueue() {
+function putQueue () {
   let movie = getQueue();
   let pushMovie = false;
   const index = movie.indexOf(id);
-  if(index === -1) {
+  if (index === -1) {
     movie.push(id);
     pushMovie = true;
-  }else{
+  } else {
     movie.splice(index, 1);
   }
-  return {movie, pushMovie }
+  return { movie, pushMovie };
 }
