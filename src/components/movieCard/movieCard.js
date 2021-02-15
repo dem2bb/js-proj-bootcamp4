239bdab57286c672c1fdb0eba2d92221f1c10767
Lougle <louglescom@gmail.com>
2021-02-15 17:45:16 +0200

    localstorage change data

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,14 +1,17 @@
 import movieCard from '../../templates/movieCard.hbs';
 const mainRef = document.querySelector('main');
 let id = [];
+let obj;
 export function apiMovieCard(movieId) {
   id = movieId;
-  console.log(id);
   const keyApi = '65999cd4dc4e9b42ad69f2cfa64d7f94';
   const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keyApi}&language=en-US`;
   return fetch(baseUrl)
   .then(res => res.json())
-  .then(movieCardMurkup)
+    .then(data => {
+      obj = data;
+      movieCardMurkup(data);
+  })
   .catch(err => console.log(err));
 }
 
@@ -41,6 +44,7 @@ function addIntoWatched(e) {
   btn.innerHTML = (btn.innerHTML === 'Added to Watched') ? btn.innerHTML = 'Add to Watched' : btn.innerHTML = 'Added to Watched';
   (btn.classList.contains('js-clicked')) ? btn.classList.remove('js-clicked') : btn.classList.add('js-clicked');
   localStorage.setItem('WatchedId', JSON.stringify(getWatchedMovie.movie));
+  localStorage.setItem('WatchedObj', JSON.stringify(getWatchedMovie.movie));
 }
 function addIntoQueue(e) {
   let getQueueMovie = putQueue();
@@ -57,23 +61,27 @@ function getQueue() {
   return [];
 }
 function getWatched() {
-  const movieStorage = localStorage.getItem('WatchedId');
-  if (movieStorage !== null) {
-    return JSON.parse(movieStorage);
+  const movieStorageID = localStorage.getItem('WatchedId');
+  const movieStorageObj = localStorage.getItem('WatchedObj');
+  if (movieStorageID !== null && movieStorageObj !== null) {
+    return JSON.parse(movieStorageID, movieStorageObj);
   }
   return [];
 }
 function putWatched() {
   let movie = getWatched();
+  let movieObj = getWatched();
+  console.log(movie);
   let pushMovie = false;
   const index = movie.indexOf(id);
   if(index === -1) {
     movie.push(id);
+    movieObj.push(obj);
     pushMovie = true;
   }else{
     movie.splice(index, 1);
   }
-  return {movie, pushMovie }
+  return {movie, pushMovie, movieObj }
 }
 function putQueue() {
   let movie = getQueue();
